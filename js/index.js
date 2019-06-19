667fa37a97cb084abbb4d346e8dca0757bce6ba6
Charbo <charbo233@qq.com>
2019-06-19 14:32:09 +0800

    新闻轮播图完成

--- js/index.js
+++ js/index.js
@@ -3,48 +3,77 @@ var year = myDate.getFullYear(); //获取完整的年份(4位,1970-????)
 var month = ("0" + (myDate.getMonth() + 1)).slice(-2); //获取当前月份(01-12)
 var date = myDate.getDate(); //获取当前日(1-31)
 var str = "星期" + "日一二三四五六".charAt(new Date().getDay());
-var bannerIndex = 0;
-var bannerList = document.querySelectorAll('.banner-item');
-var bannerDotList = document.querySelectorAll('.dot-item');
-var bannerDotTimer;
-var newIndex;
+
+//主轮播图变量
+var home_curBannerIndex = 0;
+var home_targetBannerIndex;
+var home_bannerDotTimer;
+var home_bannerSwitchTimer;
+var home_bannerList = document.querySelectorAll('.banner-item');
+var home_bannerDotList = document.querySelectorAll('.dot-item');
+
+//新闻轮播图变量
+var news_curBannerIndex = 0;
+var news_targetBannerIndex;
+var news_bannerDotTimer;
+var news_bannerSwitchTimer;
+var news_bannerList = document.querySelectorAll('.banner-item');
+var news_bannerDotList = document.querySelectorAll('.dot-item');
+
 $(function() {
+    autoSwitch(true);
     //日期加载
     $('.date').html('<div class="font1">' + date + '</div><div class="font2">' + month + '月' +
         '</div><div class="font3">' + year + '</div>');
     $('.date').addClass('date-loaded');
 
+    //主轮播图
     $('.pre').click(function() {
         
-        newIndex=bannerIndex-1;
-        newIndex = newIndex < 0 ? 4 : newIndex;
-       switchBanner(newIndex);
+        switchBanner(home_curBannerIndex - 1);
     })
     $('.next').click(function() {
-        newIndex=bannerIndex+1;
-        newIndex = newIndex > 4 ? 0 : newIndex;
-        
-        switchBanner(newIndex);
+        switchBanner(home_curBannerIndex + 1);
     })
     $('.banner-dot').on('mouseover', '.dot-item', function() {
-        var currentDot=this;
-        bannerDotTimer = setTimeout(function() {
-            
-           
-            newIndex = parseInt(currentDot.getAttribute('data-index'));
-            switchBanner(newIndex);
-        },200);
+        var currentDot = this;
+        home_bannerDotTimer = setTimeout(function() {
+            home_targetBannerIndex = parseInt(currentDot.getAttribute('data-index'));
+            switchBanner(home_targetBannerIndex);
+        }, 200);
 
     })
     $('.banner-dot').on('mouseleave', '.dot-item', function() {
-        clearTimeout(bannerDotTimer);
+        clearTimeout(home_bannerDotTimer);
 
     })
-    function switchBanner(newIndex){
-        bannerDotList[bannerIndex].classList.remove('active');
-        bannerList[bannerIndex].classList.remove('active');
-        bannerList[newIndex].classList.add('active');
-        bannerDotList[newIndex].classList.add('active');
-        bannerIndex=newIndex;
+    $('#container').on('mouseover', '.banner', function() {
+        autoSwitch(false);
+    })
+    $('#container').on('mouseleave', '.banner', function() {
+        autoSwitch(true);
+    })
+
+    function switchBanner(home_targetBannerIndex) {
+        home_targetBannerIndex = home_targetBannerIndex < 0 ? 4 : home_targetBannerIndex;
+        home_targetBannerIndex = home_targetBannerIndex > 4 ? 0 : home_targetBannerIndex;
+        home_bannerDotList[home_curBannerIndex].classList.remove('active');
+        home_bannerList[home_curBannerIndex].classList.remove('active');
+        home_bannerList[home_targetBannerIndex].classList.add('active');
+        home_bannerDotList[home_targetBannerIndex].classList.add('active');
+        home_curBannerIndex = home_targetBannerIndex;
     }
+
+    function autoSwitch(toAuto) {
+        if (toAuto) {
+            home_bannerSwitchTimer = setInterval(function() {
+                switchBanner(home_curBannerIndex + 1);
+            }, 1000);
+        } else {
+            clearInterval(home_bannerSwitchTimer);
+        }
+
+    }
+    //新闻轮播图
+
 })
